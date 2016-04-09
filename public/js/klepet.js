235ffb9a15e471ede7569056e12f489af5e6140a
Tim Rejc <tr9945@student.uni-lj.si>
2016-04-09 21:27:21 +0000

    Dodano branje ukaza dregljaj

--- public/js/klepet.js
+++ public/js/klepet.js
@@ -43,6 +43,17 @@ Klepet.prototype.procesirajUkaz = function(ukaz) {
         sporocilo = 'Neznan ukaz';
       }
       break;
+    case 'dregljaj':
+      besede.shift();
+      var prejemnik = besede.join(' ');
+      if(prejemnik) {
+        var dregljaj = {vzdevek:prejemnik};
+        this.socket.emit('dregljaj', dregljaj);
+        sporocilo = 'Dregljaj za ' + dregljaj.vzdevek;
+      }else {
+        sporocilo = 'Neznan ukaz';
+      }
+      break;
     default:
       sporocilo = 'Neznan ukaz.';
       break;
